f4f5fc0d590b4cc5bec030483fecf4c3a85156a7
LukasLg <lukas.leimgruber@mobilityhouse.com>
2017-09-20 10:54:10 +0200

    start working on card class
    added suit attribute to card class

--- tests/test_blackjack.py
+++ tests/test_blackjack.py
@@ -1,8 +1,8 @@
-from blackjack import cards, deck
+from blackjack import Card, Deck
 
-def test_cards():
-    pass
+def test_card_has_suit():
+    card = Card('diamonds')
+    assert hasattr(card, 'suit')
 
 def test_deck():
     pass
-
