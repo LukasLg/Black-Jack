58deec5e5bca9f4d6d81583309b0219d098c56b7
LukasLg <lukas.leimgruber@mobilityhouse.com>
2017-09-20 11:07:18 +0200

    start working on card class
    added suit attribute to card class
    test for suits

--- blackjack/__init__.py
+++ blackjack/__init__.py
@@ -1,6 +1,10 @@
 class Card:
     def __init__(self, suit):
+        if suit not in ('spade', 'diamond', 'heart', 'clubs'):
+            raise ValueError('Suit must be according to 52-standard card game')
+
         self.suit = suit
+
 
 class Deck:
     pass
--- tests/test_blackjack.py
+++ tests/test_blackjack.py
@@ -1,8 +1,13 @@
+import pytest
 from blackjack import Card, Deck
 
 def test_card_has_suit():
-    card = Card('diamonds')
+    card = Card('diamond')
     assert hasattr(card, 'suit')
 
 def test_deck():
     pass
+
+def test_has_valid_suit():
+    with pytest.raises(ValueError):
+        Card('fakesuit')
