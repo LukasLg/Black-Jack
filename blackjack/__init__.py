1f6af8b5ace30056bc62df5237e414609d7a572f
LukasLg <lukas.leimgruber@mobilityhouse.com>
2017-09-21 11:28:41 +0200

    start working on card class
    added suit attribute to card class
    test for suits
    tests for comparing of cards

--- blackjack/__init__.py
+++ blackjack/__init__.py
@@ -1,9 +1,30 @@
 class Card:
-    def __init__(self, suit):
+    def __init__(self, suit, value):
         if suit not in ('spade', 'diamond', 'heart', 'clubs'):
             raise ValueError('Suit must be according to 52-standard card game')
 
         self.suit = suit
+
+        if value not in ['A','K', 'Q','J'] and value not in list(range(2,11)):
+            raise ValueError
+
+        self.value = value
+
+        if value in ['K', 'Q', 'J']:
+            self.num_value = 10
+        elif value == 'A':
+            self.num_value = 11
+        else:
+            self.num_value = value
+
+    def __add__(self, other):
+        return self.num_value + other.num_value
+
+    def __eq__(self, other):
+        return self.num_value == other.num_value
+
+    def __gt__(self, other):
+        return self.num_value > other.num_value
 
 class Deck:
     pass
--- tests/test_blackjack.py
+++ tests/test_blackjack.py
@@ -2,7 +2,7 @@ import pytest
 from blackjack import Card, Deck
 
 def test_card_has_suit():
-    card = Card('diamond')
+    card = Card('diamond',2)
     assert hasattr(card, 'suit')
 
 def test_deck():
@@ -10,4 +10,28 @@ def test_deck():
 
 def test_has_valid_suit():
     with pytest.raises(ValueError):
-        Card('fakesuit')
+        Card('fakesuit',2)
+
+    c = Card('spade', 2)
+    assert c.suit == 'spade'
+
+def test_card_has_a_value():
+
+    king_of_spades = Card('spade', 'K')
+    queen_of_spades = Card('spade', 'Q')
+
+    assert king_of_spades.value == 'K'
+    assert queen_of_spades.value == 'Q'
+
+def test_add_card():
+    assert 5 == Card('spade', 2) + Card('spade', 3)
+
+def test_add_with_face():
+    assert 13 == Card('spade', 'J') + Card('spade', 3)
+
+def test_compare():
+    assert True == (Card('spade','Q') == Card('spade','J'))
+
+def test_greater_then():
+    assert True == (Card('spade','Q') > Card('spade',3))
+
